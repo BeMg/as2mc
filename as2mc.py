876224a2a8f46589f06a698754939e8d6a053685
BeMg <gccbg04538@gmail.com>
2018-07-28 19:16:44 +0800

    refactor

--- as2mc.py
+++ as2mc.py
@@ -10,7 +10,13 @@ class assembler:
     def add_insn(self, name, t, opcode, func):
         self.insn_opcode[name] = opcode << 0
         self.insn_func[name]   = func   << 12
-        self.insn_type[name]   = t
+        # Type classification by format below
+        # 1 -> name imm20
+        # 2 -> name rd, rs1
+        # 3 -> name rd, rs1, rs2
+        # 4 -> name rd, imm12(rs1)
+        # 5 -> name rd, rs1, imm12
+        self.insn_type[name]  = t
 
     def add_zero_in_ahead(self, target, l):
         while len(target) < l:
@@ -21,22 +27,39 @@ class assembler:
         encode = 0b0
         apart = [i.strip(' ,') for i in insn.split(' ')]
         name = apart[0]
-        if self.insn_type[name] == 'I':
+        type_of_insn = self.insn_type[name] 
+
+        if type_of_insn == 2:
             name, rd, rs1 = apart
-        encode = encode | self.insn_opcode[name] | self.insn_func[name] | self.reg[rd] << 7 | self.reg[rs1] << 15
+            encode = encode | self.insn_opcode[name] | self.insn_func[name] | self.reg[rd] << 7 | self.reg[rs1] << 15
+        elif type_of_insn == 1:
+            name, imm20 = apart
+            encode = encode | self.insn_opcode[name] | int(imm20) << 12
+        elif type_of_insn == 3:
+            name, rd, rs1, rs2 = apart
+            encode = encode | self.insn_opcode[name] | self.insn_func[name] | self.reg[rd] << 7 | self.reg[rs1] << 15 | self.reg[rs2] << 20
+        elif type_of_insn == 4:
+            name, rd, target = apart
+            imm12, rs1 = [i.strip(')') for i in target.split('(')]
+            encode = encode | self.insn_opcode[name] | self.insn_func[name] | self.reg[rd] << 7 | self.reg[rs1] << 15 | int(imm12) << 20
+        elif type_of_insn == 5:
+            name, rd, rs1, imm12 = apart
+            encode = encode | self.insn_opcode[name] | self.insn_func[name] | self.reg[rd] << 7 | self.reg[rs1] << 15 | int(imm12) << 20
+        else:
+            raise Exception('No this type')
+
         return encode
 
 if __name__=='__main__':
     a = assembler()
-    a.add_insn('setvl', 'I', 0b0001011, 0)
-    a.add_insn('vld'  , 'I', 0b0001011, 1)
-    a.add_insn('vadd' , 'I', 0b0001011, 2)
-    a.add_insn('vst'  , 'I', 0b0001011, 3)
+    a.add_insn('setvl', 2, 0b0001011, 0)
+    a.add_insn('vld'  , 2, 0b0001011, 1)
+    a.add_insn('vadd' , 2, 0b0001011, 2)
+    a.add_insn('vst'  , 2, 0b0001011, 3)
     code = []
     code.append('setvl a5, a5')
     code.append('vld v0, a0')
     code.append('vadd v0, v0')
     code.append('vst v0, a0')
     inline_asm = [hex(a.gen_code(i)) for i in code]
-    print(inline_asm)
     
