cb4fbdfa4af97aa2af715f41b54768c8fe87d4d2
BeMg <gccbg04538@gmail.com>
2018-07-28 19:16:58 +0800

    update

--- main.py
+++ main.py
@@ -4,10 +4,12 @@ from as2mc import assembler
 
 if __name__=='__main__':
     a = assembler()
-    a.add_insn('setvl', 'I', 0b0001011, 0)
-    a.add_insn('vld'  , 'I', 0b0001011, 1)
-    a.add_insn('vadd' , 'I', 0b0001011, 2)
-    a.add_insn('vst'  , 'I', 0b0001011, 3)
+    a.add_insn('setvl', 2, 0b0001011, 0)
+    a.add_insn('vld'  , 2, 0b0001011, 1)
+    a.add_insn('vadd' , 2, 0b0001011, 2)
+    a.add_insn('vst'  , 2, 0b0001011, 3)
+    a.add_insn('vconfig', 1, 0b0101011, -1)
+    a.add_insn('addi', 5, 0b0010011, 0)
 
     while True:
         command = input('> ')
